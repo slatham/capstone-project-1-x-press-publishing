4cd25b053583286958b58c3ec3d061e2597ce910
Stephen Latham <slatham@Stephens-iMac.local>
2018-10-30 19:43:27 +0000

    Some changes to server

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
+// set up express
 const express = require('express');
 const app = express();
+// require and use the body parser middle-ware
+const bodyParser = require('body-parser');
+app.use(bodyParser());
 
+// export app
 module.export = app;
 
+// set up where we serve static content from
+app.use(express.static('public'));
+
+
+
+// Set up Routers here.
+
+
+
+
+
+
+
+// configure the port to listen on
 const PORT = process.env.PORT || 4000;
 
-
-
-
+// start the server on the port
 app.listen(PORT,() => {
 
 console.log(`Listening on port ${PORT}`);
